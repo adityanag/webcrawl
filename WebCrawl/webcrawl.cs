b049e804511a8b28597d9b70708b32dbe2481b9a
Aditya Nag <adityanag@gmail.com>
2015-07-17 02:30:47 -0400

    Reading from file, testing the URLs and writing errors all working now.

--- WebCrawl/webcrawl.cs
+++ WebCrawl/webcrawl.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using System.IO;
 
@@ -10,50 +8,68 @@ namespace WebCrawl
 {
     class webcrawl
     {
-        private static string url;
-        private static HttpWebRequest webRequest;
-        private static HttpWebResponse response;
+        private static string[] url; //Array for user input file
+        private static HttpWebRequest webRequest; // webrequest
         private static HttpStatusCode responseStatusCode;
-        private static string[] lines;
-
+        private static List<String> errors = new List<string>(); //List of errors - it's a list because I don't know how many errors there may be
+        private static string[] errorArray; //Array of errors - I dump the list into an array before writing  errors.txt
+        private static int x=0; //This is a counter that increments independently of i - just keeps the error file neat, as it increments the list by 1 every time I write an error
 
         static void Main(string[] args)
         {
 
-            ReadArray();
-
-            Console.WriteLine("Enter the url you want to test");
-            url = Console.ReadLine();
-            webRequest = (HttpWebRequest)WebRequest.Create(url);
-
-            try
+         
+            ReadArray(); //Reads file.txt and creates an array of Urls 
+           
+            for (int i=0; i<url.Length;i++) //start to loop through the array
             {
-               
-                HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
-                responseStatusCode = response.StatusCode;
-                Console.Write((int)response.StatusCode);
-                Console.ReadKey();
+                
+                try
+                {
+                     //If response is 200 OK, continue silently
+                    webRequest = (HttpWebRequest)WebRequest.Create(url[i]);
+                    webRequest.Timeout = 500; //.5 second timeout
+                    HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+                   // responseStatusCode = response.StatusCode; // In case I want to do something with the response
+                  
+                }
 
+                catch (WebException errorCode)
+                {
+                    if (errorCode.Response == null) //Catch Null Exception - this is usually a timeout
+                        {                                         
+                        errors.Add("URL " + url[i] + " " + errorCode.Message);//Write error to the list
+                         x++;//increment list counter
+                        }
+                    else //Catch any other error - 404, 403, etc
+                    { 
+                   // responseStatusCode = ((HttpWebResponse)errorCode.Response).StatusCode;//Leaving this in case I want to do something later
+                    errors.Add("URL " + url[i] + " " + errorCode.Message);//Write error to the list
+                    x++;//increment list counter
+                     }
+                  
+
+                }
             }
 
-            catch (WebException errorCode)
-            {
-                responseStatusCode = ((HttpWebResponse)errorCode.Response).StatusCode;
-                Console.Write((int)responseStatusCode);
-                Console.ReadKey();
+            errorArray = errors.ToArray(); //Convert the list to an array - this helps with writing the error file
+            WriteErrorFile();
+            Console.WriteLine("Done! Check errors.txt for errors.");
 
-            }
-
-
-            
         }
+
+//Methods to Read from the file and write the error file follow
 
         static void ReadArray()
         {
-            lines = File.ReadLines("file.txt").ToArray();
-            Console.ReadKey();
+            url = File.ReadLines("file.txt").ToArray();
+
         }
 
+        static void WriteErrorFile()
+        {
+            System.IO.File.WriteAllLines("errors.txt", errorArray);
+        }
 
 
     }
